d6ab67c49f2919c5daccf4397728953f6a7893a0
Claus Damgaard <jinxxed@outlook.com>
2023-03-21 20:51:25 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please input a string: ");
+String toCheck = Console.ReadLine();
+
+char[] strArray = toCheck.ToCharArray();
+
+Array.Reverse(strArray);
+
+String reverseString = new string(strArray);
+
+
+if (toCheck.Equals(reverseString))
+{
+    Console.WriteLine(toCheck + " is a palindrome.");
+}
+else
+{
+    Console.WriteLine(toCheck + " is not a palindrome.");
+}
